69f8228e5c6eda4444bfc484e6fdddc36964f178
pipe34150 <pipe34150@hotmail.com>
2017-10-16 11:38:39 +0700

    :tada: Initialized Repository

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/PzHacker/Desktop/App/pages/index.js?entry';
+
+
+var Index = function Index() {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 4
+        }
+    }, ' This is SIT CRAFT Camp ');
+};
+
+exports.default = Index;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiSW5kZXgiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7Ozs7OztBQUVQLElBQU0sUUFBUSxTQUFSLEFBQVEsUUFBQTsyQkFDVixjQUFBOztzQkFBQTt3QkFBQTtBQUFBO0FBQUEsS0FBQSxFQURVLEFBQ1Y7QUFESixBQUlBOztrQkFBQSxBQUFlIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9QekhhY2tlci9EZXNrdG9wL0FwcCJ9
